38df3be9289be81dc266a47dcda7010d7c3286bb
Ciesiu <fisher94.poczta@gmail.com>
2017-06-17 20:36:37 +0200

    wincyj prefabów

---
+++ Assets/Scripts/HouseDestruction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HouseDestruction : MonoBehaviour {
+
+    public GameObject burnHouse;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Quaternion zero = new Quaternion();
+        zero.eulerAngles = new Vector3(0, 0, 0);
+        Transform burningHouse = Instantiate(burnHouse, transform.position, zero) as Transform;
+        Physics2D.IgnoreLayerCollision(8, 13);
+        Physics2D.IgnoreLayerCollision(9, 13);
+        Destroy(gameObject);
+    }
+}
