aa5219117ce53b16dafcb32de333ca93d298c4d5
Ciesiu <fisher94.poczta@gmail.com>
2017-06-18 20:15:10 +0200

    Gwiazdy

---
+++ Assets/Scripts/StarScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarScript : MonoBehaviour {
+
+    Animator animator;
+    System.Random rnd;
+
+    // Use this for initialization
+    void Start () {
+        rnd = new System.Random((int)this.transform.position.y);
+        animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //rnd = new System.Random();
+        animator.SetInteger("starFactor", rnd.Next(0,4));
+	}
+}
